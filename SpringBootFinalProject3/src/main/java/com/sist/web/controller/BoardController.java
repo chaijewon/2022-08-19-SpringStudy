01101ec5fe534c76287b4c9671c91f136fae6b09
user <user@DESKTOP-COJ9858>
2022-10-12 15:30:15 +0900

    1111

--- SpringBootFinalProject3/src/main/java/com/sist/web/controller/BoardController.java
+++ SpringBootFinalProject3/src/main/java/com/sist/web/controller/BoardController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.sist.web.dao.*;
 import com.sist.web.entity.*;
@@ -30,7 +31,8 @@ public class BoardController {
 	   model.addAttribute("curpage", curpage);
 	   model.addAttribute("totalpage", totalpage);
 	   model.addAttribute("list", list);
-	   return "board/list";
+	   model.addAttribute("main_content", "board/list");
+	   return "main";
    }
    // JSP => HTML (Thymeleaf,Vue,React), DAO
    /*
@@ -70,19 +72,49 @@ public class BoardController {
 	   
 	   vo=dao.findByNo(no);
 	   model.addAttribute("vo", vo); // EL => ${} 
-	   return "board/detail";
+	   model.addAttribute("main_content", "board/detail");
+	   return "main";
    }
    
    @GetMapping("/board/insert")
-   public String board_insert()
+   public String board_insert(Model model)
    {
-	   return "board/insert";
+	   model.addAttribute("main_content", "board/insert");
+	   return "main";
    }
    
    @PostMapping("/board/insert_ok")
    public String board_insert_ok(BoardEntity vo)
    {
 	   dao.save(vo);
+	   return "redirect:/board/list";
+   }
+   
+   @GetMapping("/board/update")
+   public String board_update(int no,Model model)
+   {
+	   BoardEntity vo=dao.findByNo(no);
+	   model.addAttribute("vo", vo);
+	   model.addAttribute("main_content","board/update");
+	   return "main";
+   }
+   
+   @PostMapping("/board/update_ok")
+   public String board_update_ok(BoardEntity vo,RedirectAttributes ra)
+   {
+	   BoardEntity pvo=dao.findByNo(vo.getNo());
+	   vo.setHit(pvo.getHit());
+	   vo.setPwd(pvo.getPwd());
+	   dao.save(vo);//update
+	   ra.addAttribute("no", vo.getNo());
+	   return "redirect:/board/detail";
+   }
+   
+   @GetMapping("/board/delete")
+   public String board_delete(int no)
+   {
+	   BoardEntity vo=dao.findByNo(no);
+	   dao.delete(vo);
 	   return "redirect:/board/list";
    }
 }
